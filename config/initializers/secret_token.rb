616a9b4ab872a46d4dc5bdde65da11e2ba0eb434
Edward Rudd <urkle@outoforder.cc>
2012-01-22 16:54:37 -0500

    add in initial rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumbleBugs::Application.config.secret_token = '1d1452b4d6128f30222f445b596cabdef81af81b5aa7787878032280a6b94377e112d71193d8401d755e80055d9eb5ca7a69d0222e8b4a4b801dc7dd7635cd4d'
