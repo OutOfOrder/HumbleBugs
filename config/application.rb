675d0515f719163a7aefe8c91299bb7ea7a6d006
Edward Rudd <urkle@outoforder.cc>
2012-01-29 17:58:56 -0500

    disable initialization on asset precompile (fixes heroku deploy)

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module HumbleBugs
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't Initialize on precompile?
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
