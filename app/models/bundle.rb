d59c12656fcff7204dc7a463000d99e737750597
Edward Rudd <urkle@outoforder.cc>
2012-01-30 11:30:27 -0500

    rework bundle forms and add list of states

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,3 +1,14 @@
 class Bundle < ActiveRecord::Base
   has_many :games
+
+  STATES = [
+      ['Planned', :planned],
+      ['In Development', :development],
+      ['Pending Release', :pending],
+      ['Active', :active],
+      ['Completed', :completed]
+  ]
+
+  validates_presence_of :name, :state, :description
+  validates_inclusion_of :state, :in => STATES.map { |m| m.second.to_s }, :message => "state %{value} is not a valid state"
 end
