b610b9caea306eee208f57a0b87ff0256140f18c
Edward Rudd <urkle@outoforder.cc>
2012-02-14 16:47:06 -0500

    define game states and validators

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,14 @@
 class Game < ActiveRecord::Base
   belongs_to :bundle
+
+  STATES = [
+      ['Prospective', :prospective],
+      ['Planned', :planned],
+      ['In Development', :development],
+      ['QA Testing', :testing],
+      ['Completed', :completed]
+  ]
+
+  validates_presence_of :name, :state, :description
+  validates_inclusion_of :state, :in => STATES.map { |m| m.second.to_s }, :message => "state %{value} is not a valid state"
 end
