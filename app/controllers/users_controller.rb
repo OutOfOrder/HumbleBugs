ff0e924e6db0aeab5a82e596d54b4e3c75bfeab7
Edward Rudd <urkle@outoforder.cc>
2012-05-31 10:54:30 -0400

    apply decl_auth to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  filter_resource_access
+
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.with_permissions_to
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,8 +15,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
@@ -24,8 +24,6 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
@@ -34,14 +32,11 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to login_path, notice: 'Signed Up!' }
@@ -56,8 +51,6 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -72,7 +65,6 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     respond_to do |format|
