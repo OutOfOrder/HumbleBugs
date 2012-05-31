3a8747f4c4a58bc1944bad0414f677e751d0377c
Edward Rudd <urkle@outoforder.cc>
2012-05-31 08:48:17 -0400

    use decl_auth before filters and with_permissions
    - also clear out unneeded cruft from spec

--- app/controllers/bundles_controller.rb
+++ app/controllers/bundles_controller.rb
@@ -4,7 +4,7 @@ class BundlesController < ApplicationController
   # GET /bundles
   # GET /bundles.json
   def index
-    @bundles = Bundle.all
+    @bundles = Bundle.with_permissions_to
 
     respond_to do |format|
       format.html # index.html.erb
@@ -15,8 +15,6 @@ class BundlesController < ApplicationController
   # GET /bundles/1
   # GET /bundles/1.json
   def show
-    @bundle = Bundle.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @bundle }
@@ -26,8 +24,6 @@ class BundlesController < ApplicationController
   # GET /bundles/new
   # GET /bundles/new.json
   def new
-    @bundle = Bundle.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @bundle }
@@ -36,14 +32,11 @@ class BundlesController < ApplicationController
 
   # GET /bundles/1/edit
   def edit
-    @bundle = Bundle.find(params[:id])
   end
 
   # POST /bundles
   # POST /bundles.json
   def create
-    @bundle = Bundle.new(params[:bundle])
-
     respond_to do |format|
       if @bundle.save
         format.html { redirect_to @bundle, notice: 'Bundle was successfully created.' }
@@ -58,8 +51,6 @@ class BundlesController < ApplicationController
   # PUT /bundles/1
   # PUT /bundles/1.json
   def update
-    @bundle = Bundle.find(params[:id])
-
     respond_to do |format|
       if @bundle.update_attributes(params[:bundle])
         format.html { redirect_to @bundle, notice: 'Bundle was successfully updated.' }
@@ -74,7 +65,6 @@ class BundlesController < ApplicationController
   # DELETE /bundles/1
   # DELETE /bundles/1.json
   def destroy
-    @bundle = Bundle.find(params[:id])
     @bundle.destroy
 
     respond_to do |format|
