2a5b0caf1be74b97db805a0af53ae4ce0f149aad
Edward Rudd <urkle@outoforder.cc>
2012-06-02 12:28:51 -0400

    add decl_auth to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,9 @@
 class NotesController < ApplicationController
-  before_filter :load_noteable
-
+  filter_resource_access :nested_in => :noteable
   # GET /:polymorphic/1/notes
   # GET /:polymorphic/1/notes.json
   def index
-    @notes = @noteable.notes
+    @notes = @noteable.notes.with_permissions_to
 
     respond_to do |format|
       format.html # index.html.erb
@@ -15,8 +14,6 @@ class NotesController < ApplicationController
   # GET /:polymorphic/1/notes/1
   # GET /:polymorphic/1/notes/1.json
   def show
-    @note = @noteable.notes.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @note }
@@ -26,8 +23,6 @@ class NotesController < ApplicationController
   # GET /:polymorphic/1/notes/new
   # GET /:polymorphic/1/notes/new.json
   def new
-    @note = @noteable.notes.build
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @note }
@@ -36,14 +31,11 @@ class NotesController < ApplicationController
 
   # GET /:polymorphic/1/notes/1/edit
   def edit
-    @note = @noteable.notes.find(params[:id])
   end
 
   # POST /:polymorphic/1/notes
   # POST /:polymorphic/1/notes.json
   def create
-    @note = @noteable.notes.build(params[:note])
-
     respond_to do |format|
       if @note.save
         format.html { redirect_to [@noteable, @note], notice: 'Note was successfully created.' }
@@ -58,8 +50,6 @@ class NotesController < ApplicationController
   # PUT /:polymorphic/1/notes/1
   # PUT /:polymorphic/1/notes/1.json
   def update
-    @note = @noteable.notes.find(params[:id])
-
     respond_to do |format|
       if @note.update_attributes(params[:note])
         format.html { redirect_to [@noteable, @note], notice: 'Note was successfully updated.' }
@@ -74,7 +64,6 @@ class NotesController < ApplicationController
   # DELETE /:polymorphic/1/notes/1
   # DELETE /:polymorphic/1/notes/1.json
   def destroy
-    @note = @noteable.notes.find(params[:id])
     @note.destroy
 
     respond_to do |format|
@@ -83,10 +72,12 @@ class NotesController < ApplicationController
     end
   end
 
-private
+protected
   def load_noteable
     @noteable = find_polymorphic
   end
+
+private
   def find_polymorphic
     params.each do |name, value|
       if name.ends_with?("_id")
