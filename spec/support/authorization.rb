255789ec522611e99924df5340b9f45e60045983
Edward Rudd <urkle@outoforder.cc>
2012-06-12 23:17:28 -0400

    fix rspec test for views with decl_auth

--- spec/support/authorization.rb
+++ spec/support/authorization.rb
@@ -1,3 +1,16 @@
 require 'declarative_authorization/maintenance'
 
 include Authorization::TestHelper
+
+RSpec.configure do |c|
+  c.before :each, :type => :view do
+    controller.stub(:current_user).and_return(nil)
+  end
+end
+
+def render_with user, options = {}, local_assigns = {}, &block
+  Authorization::Maintenance.with_user(user) do
+    controller.stub(:current_user).and_return(user)
+    render options, local_assigns, &block
+  end
+end
