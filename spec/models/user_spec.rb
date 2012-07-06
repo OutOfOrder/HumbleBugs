259354222949e8820e73ec02f6462df99ada2a93
Edward Rudd <urkle@outoforder.cc>
2012-07-06 15:16:02 -0400

    add in validation on adding in duplicate emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe 'scopes' do
     describe '#with_role' do
       it 'should return users in the scope specified' do
@@ -10,6 +8,18 @@ describe User do
         FactoryGirl.create_list :user, 2, :roles => [:other]
         User.with_role(:good).should =~ users
       end
+    end
+  end
+
+  describe 'validations' do
+    it 'duplicate mixed case emails' do
+      user = FactoryGirl.create :user
+
+      user2 = FactoryGirl.build :user, :email => user.email.upcase
+
+      user2.should have(1).errors_on(:email)
+
+      user.email.should_not == user2.email
     end
   end
 end
