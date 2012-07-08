eacde6074243f6355fb2ac5bbaa9ddadb1ba197d
Edward Rudd <urkle@outoforder.cc>
2012-07-08 13:56:32 -0400

    a tester can not see the prospective bundle of a game in testing

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 describe "games/show" do
-  before(:each) do
-    @game = assign(:game, FactoryGirl.create(:game))
-  end
-
   it "renders attributes in <p>" do
+    assign(:game, FactoryGirl.create(:game))
     render
     rendered.should match(/Name/)
     rendered.should match(/Pong Game/)
     rendered.should match(/Prospective/)
+  end
+
+  it 'a tester can not see the bundle of a game in testing' do
+    user = FactoryGirl.create :user, :roles => [:tester]
+    assign(:game, FactoryGirl.create(:game, :testing))
+
+    render_with user
+    rendered.should match(/Name/)
+    rendered.should_not match(/Bundle:/)
   end
 end
