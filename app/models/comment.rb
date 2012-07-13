d91acd348284d2bf9442b072de8ce13337679261
Edward Rudd <urkle@outoforder.cc>
2012-07-13 11:34:02 -0400

    setup inverse definitions for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => 'User'
-  belongs_to :commentable, :polymorphic => true
+  belongs_to :commentable, :polymorphic => true, :inverse_of => :comments
 
   validates_presence_of :commentable, :author
 end
