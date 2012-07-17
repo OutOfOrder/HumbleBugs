5268fea97c1e5fd5cb7669dfc2b6dec7bde91e22
Edward Rudd <urkle@outoforder.cc>
2012-07-16 20:48:43 -0400

    add user creation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-PredefinedTag.create([
-    {context:'platforms', name:'Windows'},
-    {context:'platforms', name:'Mac OS X'},
-    {context:'platforms', name:'Linux x86'},
-    {context:'platforms', name:'Linux x86_64'},
-    {context:'platforms', name:'Android'}
-]);
+PredefinedTag.create!([
+                          {context: 'platforms', name: 'Windows'},
+                          {context: 'platforms', name: 'Mac OS X'},
+                          {context: 'platforms', name: 'Linux x86'},
+                          {context: 'platforms', name: 'Linux x86_64'},
+                          {context: 'platforms', name: 'Android'}
+                      ]);
+
+if Rails.env.development?
+  Authorization::Engine.instance.role_titles.each do |role, title|
+    u = User.create!(name: title, email: "#{role.to_s}@example.com", password: "test", password_confirmation: "test")
+    u.roles.create! role: role.to_s
+  end
+end
