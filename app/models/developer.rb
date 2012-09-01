1e758b410399300bdfa11c1dc7620c1a23d8219c
Edward Rudd <urkle@outoforder.cc>
2012-08-31 23:48:12 -0400

    fix issue in validation for developer website

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -7,7 +7,7 @@ class Developer < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_format_of :website, :with => URI::regexp(%w(http https)), allow_nil: true
+  validates_format_of :website, :with => URI::regexp(%w(http https)), allow_blank: true
 
   before_save :set_null_fields
 
