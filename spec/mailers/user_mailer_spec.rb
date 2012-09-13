563e0b7b3cd6d156c95c78155579e6090de580f8
Edward Rudd <urkle@outoforder.cc>
2012-09-13 12:48:41 -0400

    slightly adjust user mailer spec to not incidentally use global vars

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,32 +2,36 @@ require "spec_helper"
 
 describe UserMailer do
   describe "password_reset" do
-    user = FactoryGirl.create(:user, password_reset_token: 'testtoken')
-    let(:mail) { UserMailer.password_reset(user) }
+    before do
+      @user = FactoryGirl.create(:user, password_reset_token: 'testtoken')
+      @mail = UserMailer.password_reset(@user)
+    end
 
     it "renders the headers" do
-      mail.subject.should eq("Reset your HumbleBugs account password")
-      mail.to.should eq([user.email])
-      mail.from.should eq(["myfromaddress@example.com"])
+      @mail.subject.should eq("Reset your HumbleBugs account password")
+      @mail.to.should eq([@user.email])
+      @mail.from.should eq(["myfromaddress@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("/forgot_password/testtoken")
+      @mail.body.encoded.should match("/forgot_password/testtoken")
     end
   end
 
   describe "confirm_account" do
-    user = FactoryGirl.create(:user, confirm_account_token: 'testtoken')
-    let(:mail) { UserMailer.confirm_account(user) }
+    before do
+      @user = FactoryGirl.create(:user, confirm_account_token: 'testtoken')
+      @mail = UserMailer.confirm_account(@user)
+    end
 
     it "renders the headers" do
-      mail.subject.should eq("Confirm your HumbleBugs account")
-      mail.to.should eq([user.email])
-      mail.from.should eq(["myfromaddress@example.com"])
+      @mail.subject.should eq("Confirm your HumbleBugs account")
+      @mail.to.should eq([@user.email])
+      @mail.from.should eq(["myfromaddress@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("/confirm_account/testtoken")
+      @mail.body.encoded.should match("/confirm_account/testtoken")
     end
   end
 end
