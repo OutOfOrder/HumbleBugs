166d24a0bd5dfc52647431249bb224eff740081c
Edward Rudd <urkle@outoforder.cc>
2013-02-12 18:07:46 -0500

    make smtp config more dynamic (to switch providers)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,12 +69,12 @@ HumbleBugs::Application.configure do
       host: 'bugs.humblebundle.com'
   }
   config.action_mailer.smtp_settings = {
-      :address => "smtp.sendgrid.net",
+      :address => ENV['SMTP_HOST'],
       :port => 587,
       :authentication => :plain,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => "heroku.com",
+      :user_name => ENV['SMTP_USERNAME'],
+      :password => ENV['SMTP_PASSWORD'],
+      :domain => ENV['SMTP_DOMAIN'],
       :enable_starttls_auto => true
   }
 end
