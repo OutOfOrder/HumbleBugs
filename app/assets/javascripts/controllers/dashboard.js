bc1fcbeaa174a75a4a8bbd3a53355b15b978f01d
Edward Rudd <urkle@outoforder.cc>
2013-04-07 21:22:03 -0400

    initial JS code to serialize the dashboard layout

--- app/assets/javascripts/controllers/dashboard.js
+++ app/assets/javascripts/controllers/dashboard.js
@@ -1,8 +1,32 @@
-$(function() {
-  $('div.dashboard_column').sortable({
-    connectWith: 'div.dashboard_column',
-    distance: 5,
-    cursor: 'crosshair',
-    cancel: '.content'
-  });
-});
+(function() {
+    function mapConfig() {
+        return $(this).data('widget');
+    }
+
+    window.Dashboard = {
+        serializeColumn: function(col) {
+            return $('div.dashboard_column:nth-child('+col+') fieldset.widget').map(mapConfig).get();
+        },
+        serialize: function() {
+            return [
+                Dashboard.serializeColumn(1),
+                Dashboard.serializeColumn(2),
+                Dashboard.serializeColumn(3)
+            ];
+        },
+        saveDashboard: function() {
+            var config = Dashboard.serialize();
+            console.log('Dashboard Change', config);
+        }
+    };
+
+    $(function() {
+      $('div.dashboard_column').sortable({
+        connectWith: 'div.dashboard_column',
+        distance: 5,
+        cursor: 'crosshair',
+        cancel: '.content',
+        update: Dashboard.saveDashboard
+      });
+    });
+})();
