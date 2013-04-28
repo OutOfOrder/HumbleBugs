a7846f5fa90a7492696f8d450b722b618f01c07c
Edward Rudd <urkle@outoforder.cc>
2013-04-27 20:47:11 -0400

    add "zoom" button to comments to better read details

--- app/assets/javascripts/controllers/comments.js
+++ app/assets/javascripts/controllers/comments.js
@@ -1,3 +1,21 @@
 $(function() {
   $('div.comment:target').effect('pulsate', {times:2}, 'slow');
+  $('#comments').on('click', 'div.comment span.expand', function(e) {
+    console.log('click me');
+    var $comment = $(this).closest('.comment').find('.comment_entry');
+    var $dialog = $comment.clone();
+
+    $dialog.dialog({
+      autoOpen: false,
+      modal: true,
+      resizable: false,
+      show: 'scale',
+      hide: 'scale',
+      width: '90%',
+      height: $(window).height() * 0.8,
+      close: function(event, ui) {
+        $(this).remove();
+      }
+    }).dialog('open');
+  });
 });
