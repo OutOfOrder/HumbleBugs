b7ad3bbeae5b29b710289240b8e2ba370689ea33
Edward Rudd <urkle@outoforder.cc>
2013-04-29 19:02:30 -0400

    fix feedback mailer spec to not "leak" a user

--- spec/mailers/feedback_mailer_spec.rb
+++ spec/mailers/feedback_mailer_spec.rb
@@ -2,18 +2,20 @@ require "spec_helper"
 
 describe FeedbackMailer do
   describe "submit_feedback" do
-    user = FactoryGirl.create(:user)
-    feedback = FactoryGirl.build(:feedback)
-    let(:mail) { FeedbackMailer.submit_feedback(user, feedback) }
+    before do
+      @user = FactoryGirl.create(:user)
+      @feedback = FactoryGirl.build(:feedback)
+      @mail = FeedbackMailer.submit_feedback(@user, @feedback)
+    end
 
     it "renders the headers" do
-      mail.subject.should eq("HumbleBugs feedback")
-      mail.to.should eq(['feedback@example.com'])
-      mail.from.should eq(["myfromaddress@example.com"])
+      @mail.subject.should eq("HumbleBugs feedback")
+      @mail.to.should eq(['feedback@example.com'])
+      @mail.from.should eq(["myfromaddress@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match(user_path(user))
+      @mail.body.encoded.should match(user_path(@user))
     end
   end
 
