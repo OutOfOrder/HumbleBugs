5deb2c6787ce0f2ebe776332f0114c94ba898a7f
Edward Rudd <urkle@outoforder.cc>
2015-03-31 11:41:36 -0400

    update view spec to account for new list.js structure

--- spec/views/issues/index.html.erb_spec.rb
+++ spec/views/issues/index.html.erb_spec.rb
@@ -8,7 +8,7 @@ describe "issues/index" do
 
   it "renders a list of issues" do
     render
-    assert_select "tr>td", :text => @issues.first.summary, :count => 2
-    assert_select "tr>td", :text => "New".to_s, :count => 2
+    assert_select "tr>td.summary", :text => /#{@issues.first.summary}/, :count => 2
+    assert_select "tr span.status", :text => @issues.first.status, :count => 2
   end
 end
