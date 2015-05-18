886ae3318b3506d27bf662d12ded6bfe624340f6
Edward Rudd <urkle@outoforder.cc>
2015-05-18 14:55:15 -0400

    use ENV to define the secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HumbleBugs::Application.config.secret_token = '1d1452b4d6128f30222f445b596cabdef81af81b5aa7787878032280a6b94377e112d71193d8401d755e80055d9eb5ca7a69d0222e8b4a4b801dc7dd7635cd4d'
+HumbleBugs::Application.config.secret_token = ENV['SECRET_TOKEN'] || SecureRandom.urlsafe_base64(60)
+
